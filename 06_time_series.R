715ff845106f74c17e70d64f6ab59b5e7c5091a9
alvarezzariana <162352512+alvarezzariana@users.noreply.github.com>
2024-04-11 16:45:46 +0200

    Create 06_time_series.R

---
+++ 06_time_series.R
@@ -0,0 +1,35 @@
+# time series analysis
+
+library(imageRy)
+library(terra)
+
+im.list()
+
+# import the data
+EN01 <- im.import("EN_01.png")
+EN13 <- im.import("EN_13.png")
+
+par(mfrow=c(2,1))
+im.plotRGB.auto(EN01)
+im.plotRGB.auto(EN13)
+
+
+# using the first element (band) of images
+dif = EN01[[1]] - EN13[[1]]
+
+# palette
+cldif <- colorRampPalette(c("blue", "white", "red")) (100)
+plot(dif, col=cldif)
+
+
+### New example: temperature in Greenland
+
+g2000 <- im.import("greenland.2000.tif")
+clg <- colorRampPalette(c("black", "blue", "white", "red")) (100)
+plot(g2000, col=clg)
+
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+
+plot(g2015, col=clg)
