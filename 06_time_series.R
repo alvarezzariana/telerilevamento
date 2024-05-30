b1f0dec6974b965db60d7c529951a7c6a1be9776
alvarezzariana <162352512+alvarezzariana@users.noreply.github.com>
2024-05-30 18:29:15 +0200

    Update 06_time_series.R

--- 06_time_series.R
+++ 06_time_series.R
@@ -33,3 +33,19 @@ g2010 <- im.import("greenland.2010.tif")
 g2015 <- im.import("greenland.2015.tif")
 
 plot(g2015, col=clg)
+
+par(mfrow=c(1,2))
+plot(g2000, col=clg)
+plot(g2015, col=clg)
+
+# stacking the data
+stackg <- c(g2000, g2005, g2010, g2015)
+plot(stackg, col=clg)
+
+# Exercise: make the differencxe between the first and the final elemnts of the stack
+difg <- stackg[[1]] - stackg[[4]]
+# difg <- g2000 - g2015
+plot(difg, col=cldif)
+
+# Exercise: make a RGB plot using different years
+im.plotRGB(stackg, r=1, g=2, b=3)
